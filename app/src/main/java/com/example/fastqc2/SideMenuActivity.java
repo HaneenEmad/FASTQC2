5fa7f388ff6c5a306211c109d2da630fbb5e5218
HaneenEmad <haneen.emad2000@gmail.com>
2021-01-16 19:43:41 +0200

    Initial commit

---
+++ app/src/main/java/com/example/fastqc2/SideMenuActivity.java
@@ -0,0 +1,14 @@
+package com.example.fastqc2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SideMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_side_menu);
+    }
+}
